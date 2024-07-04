8060b78068d5131dea68813c0510ab4886a184cb
Arvind <ranaarvind156@gmail.com>
2024-07-04 14:46:28 +0530

    Maven

---
+++ 000. Introduction to Spring Boot/myapp/myapp/src/main/java/com/springboot/demo/myapp/controller/FunRestController.java
@@ -0,0 +1,14 @@
+package com.springboot.demo.myapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that returns "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}

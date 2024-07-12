b379e137fa9901dcacb7629c2975cf6b0be353fe
Arvind <ranaarvind156@gmail.com>
2024-07-12 14:49:46 +0530

    JPA Annotations

--- 002. Hibernate & JPA/cruddemo/src/main/java/com/myapp/cruddemo/CruddemoApplication.java
+++ 002. Hibernate & JPA/cruddemo/src/main/java/com/myapp/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.myapp.cruddemo;
 
+import com.myapp.cruddemo.dao.StudentDAO;
+import com.myapp.cruddemo.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,12 +11,29 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CruddemoApplication implements CommandLineRunner {
 
+	private StudentDAO studentDAO;
+
+	@Autowired
+	public CruddemoApplication(StudentDAO studentDAO) {
+		this.studentDAO = studentDAO;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
+
+		// Create a student object
+		System.out.println("Creating a new student object....");
+		Student tempStudent = new Student("Paul", "Doe", "paul@google.com");
+
+		// Save the student object in the database
+		System.out.println("Saving the student....");
+		studentDAO.save(tempStudent);
+
+		// Display the id of the saved student
+		System.out.println("Saved student. Generated id: "+ tempStudent.getId());
 	}
 }
---
+++ 002. Hibernate & JPA/cruddemo/src/main/java/com/myapp/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.myapp.cruddemo.dao;
+
+import com.myapp.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student student);
+}

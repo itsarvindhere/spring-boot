44d164735251cbb97cba45866c2af6c912be3bce
Arvind <ranaarvind156@gmail.com>
2024-07-16 18:12:04 +0530

    Spring Boot Rest Path Variables

--- 003. REST CRUD APIs/demo/src/main/java/com/restcrudapp/demo/controller/StudentRestController.java
+++ 003. REST CRUD APIs/demo/src/main/java/com/restcrudapp/demo/controller/StudentRestController.java
@@ -2,7 +2,9 @@ package com.restcrudapp.demo.controller;
 
 
 import com.restcrudapp.demo.pojo.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +15,28 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // Define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    // Load the student data using @PostContruct as it is called only once
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
 
         students.add(new Student("John", "Doe"));
         students.add(new Student("James", "Cook"));
         students.add(new Student("Robb", "Stark"));
+    }
 
+    // Define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+
+    // Define endpoint for retrieving a single student
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable(value = "studentId") int id) {
+        return students.get(id);
     }
 }

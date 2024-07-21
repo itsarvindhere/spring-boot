802ca90d650932a36ab6310d7a764a349b062858
Arvind <ranaarvind156@gmail.com>
2024-07-21 20:10:44 +0530

    Adding methods to Add, Update and Delete Employees in the DAO Layer

--- 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/dao/EmployeeDAO.java
+++ 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/dao/EmployeeDAO.java
@@ -8,4 +8,13 @@ public interface EmployeeDAO {
 
     // Get a list of all the employees
     List<Employee> findAll();
+
+    // Get a specific employee by id
+    Employee findById(int id);
+
+    // Add a new employee if it does not already exist or update if it does
+    Employee save(Employee employee);
+
+    // Delete an Employee
+    void delete(int id);
 }
--- 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -27,5 +28,27 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
 
         // Return the result list
         return query.getResultList();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return entityManager.merge(employee);
+    }
+
+    @Override
+    public void delete(int id) {
+
+        // Find the employee with the specific id in the database
+        Employee employee = entityManager.find(Employee.class, id);
+
+        // Delete the employee if it exists
+        if (null != employee) {
+            entityManager.remove(employee);
+        }
     }
 }

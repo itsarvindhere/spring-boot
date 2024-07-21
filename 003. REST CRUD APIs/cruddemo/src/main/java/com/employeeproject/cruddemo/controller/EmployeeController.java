9ad7854ff56681a5200b67579e8178616a4d2841
Arvind <ranaarvind156@gmail.com>
2024-07-21 21:01:31 +0530

    Spring Boot Controller - Add, Update, Delete

--- 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/controller/EmployeeController.java
+++ 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/controller/EmployeeController.java
@@ -1,11 +1,10 @@
 package com.employeeproject.cruddemo.controller;
 
 import com.employeeproject.cruddemo.entity.Employee;
+import com.employeeproject.cruddemo.exception.EmployeeNotFoundException;
 import com.employeeproject.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,50 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         return employeeService.findAll();
+    }
+
+    // Expose "/employees/id" and return the employee that has the specific id
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable(value = "id") int id) {
+        Employee employee = employeeService.findById(id);
+
+        // If the employee does not exist with the passed id, then throw an exception
+        if (null == employee) {
+            throw new EmployeeNotFoundException("Employee id not found: " + id);
+        }
+
+        return employee;
+    }
+
+    // Expose "/employees" and add the employee to the database
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    // Expose "/employees" and update the employee in the database
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        // Call the service method to update the employee
+        return employeeService.save(employee);
+    }
+
+    // Expose "/employees/id" and delete the employee from the database
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable(value = "id") int id) {
+
+        // Find the employee with the specific id
+        Employee employee = employeeService.findById(id);
+
+        // If the employee does not exist with the passed id, then throw an exception
+        if (null == employee) {
+            throw new EmployeeNotFoundException("Employee id not found: " + id);
+        }
+
+        // Call the service method to delete the employee
+        employeeService.delete(employee);
+
+        return "Employee with id " + id + " deleted!";
     }
 }

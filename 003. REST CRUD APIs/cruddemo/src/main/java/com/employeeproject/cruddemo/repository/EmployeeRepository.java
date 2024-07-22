1841233121500cb8e46f859bad94fbfadedf8512
Arvind <ranaarvind156@gmail.com>
2024-07-22 13:18:05 +0530

    Spring Data Rest

--- 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/repository/EmployeeRepository.java
+++ 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.employeeproject.cruddemo.repository;
 
 import com.employeeproject.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }

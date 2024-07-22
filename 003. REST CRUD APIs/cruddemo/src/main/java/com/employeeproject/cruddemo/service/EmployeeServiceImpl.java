4e73f2017331920558b29000deae00e2760fd16b
Arvind <ranaarvind156@gmail.com>
2024-07-22 11:56:11 +0530

    Spring Data JPA Implementation

--- 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/service/EmployeeServiceImpl.java
+++ 003. REST CRUD APIs/cruddemo/src/main/java/com/employeeproject/cruddemo/service/EmployeeServiceImpl.java
@@ -4,7 +4,6 @@ import com.employeeproject.cruddemo.entity.Employee;
 import com.employeeproject.cruddemo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,13 +29,11 @@ public class EmployeeServiceImpl implements EmployeeService{
     }
 
     @Override
-    @Transactional
     public Employee save(Employee employee) {
         return employeeRepository.save(employee);
     }
 
     @Override
-    @Transactional
     public void delete(Employee employee) {
         employeeRepository.delete(employee);
     }
